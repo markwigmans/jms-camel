51026535516a6f29e67b1cfb99fa3f003055631d
markwigmans <mark.wigmans@gmail.com>
2024-05-01 11:36:32 +0200

    Another attempt to move to camel 4.5.0

--- src/main/java/com/btb/jmscamel/RedisConfig.java
+++ src/main/java/com/btb/jmscamel/RedisConfig.java
@@ -1,5 +1,6 @@
 package com.btb.jmscamel;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -8,6 +9,7 @@ import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 
 @Configuration
+@Slf4j
 public class RedisConfig {
 
     @Value("${jc.redis.host:localhost}")
@@ -15,6 +17,7 @@ public class RedisConfig {
 
     @Bean
     public RedisConnectionFactory jedisConnectionFactory() {
+        log.error("Redis host: {}", host);
         return new JedisConnectionFactory(new RedisStandaloneConfiguration(host, 6379));
     }
 }
